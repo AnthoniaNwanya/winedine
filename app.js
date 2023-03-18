33b4ee18c8c91052e8e99d36971e8d3cbed0ae4b
Anthonia Nwanya <tonianwanya@gmail.com~>
2023-03-18 19:09:29 +0100

    remove date log

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ var today = new Date();
 var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
 var time = today.getHours() + ":" + today.getMinutes();
 var dateTime = date+' '+time;
-console.log(dateTime)
+
 class POrder {
   constructor(order, total) {
     this.date = dateTime;
